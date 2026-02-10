cabe22f79b92367921780aad3d2f2bbb0bd55fd9
samuel gracian <samuel.angeles641217@gmail.com>
2026-02-10 12:30:53 -0600

    Refactor weapon base and add firing system
    Remove old simple BaseWeapon files and replace with a refactored ABaseWeapon implementation. The header was reworked to define ABaseWeapon (now an APawn), a firing-mode enum, configurable weapon stats (magazine size, RPM, damage, range, spread, melee), components, and internal state (timers, ammo counters, reload state). The .cpp implements firing logic: CanFire, StartFire/StopFire, HandleFire, FireOnce, burst/automatic timers, PerformHit using line traces and ApplyPointDamage, ConsumeAmmo, and related helpers. Added necessary engine includes and initialized defaults (e.g. CurrentAmmoInMagazine). Overall this implements a basic ranged/melee firing system and replaces the prior placeholder actor implementation.

--- Source/HotlineMiami3D/Private/BaseWeapons.cpp
+++ Source/HotlineMiami3D/Private/BaseWeapons.cpp
@@ -1,12 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "BaseWeapons.h"
+#include "TimerManager.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
-// Sets default values
-ABaseWeapons::ABaseWeapons()
+ABaseWeapon::ABaseWeapon()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	/// --MESH-- ///
@@ -19,19 +20,184 @@ ABaseWeapons::ABaseWeapons()
 		MeshComponent->SetStaticMesh(MeshAsset.Object);
 	}
 
+	// sensible defaults
+	CurrentAmmoInMagazine = MagazineSize;
 }
 
-// Called when the game starts or when spawned
-void ABaseWeapons::BeginPlay()
+void ABaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
-void ABaseWeapons::Tick(float DeltaTime)
+void ABaseWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
 
+void ABaseWeapon::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+
+bool ABaseWeapon::CanFire() const
+{
+	if (bIsReloading)
+	{
+		return false;
+	}
+
+	if (FiringMode == EWeaponFiringMode::Melee)
+	{
+		return true;
+	}
+
+	if (bInfiniteAmmo)
+	{
+		return true;
+	}
+
+	return CurrentAmmoInMagazine > 0;
+}
+
+void ABaseWeapon::StartFire()
+{
+	bWantsToFire = true;
+
+	if (!CanFire())
+	{
+		return;
+	}
+
+	switch (FiringMode)
+	{
+	case EWeaponFiringMode::Automatic:
+	{
+		// start timer to repeatedly call HandleFire
+		float Interval = GetFireIntervalSeconds();
+		GetWorldTimerManager().SetTimer(TimerHandle_AutoFire, this, &ABaseWeapon::HandleFire, Interval, true, 0.0f);
+		// Fire immediately
+		HandleFire();
+		break;
+	}
+	case EWeaponFiringMode::SemiAuto:
+	{
+
+		FireOnce();
+		break;
+	}
+	case EWeaponFiringMode::Burst:
+	{
+		CurrentBurstShot = 0;
+		HandleFire(); 
+		break;
+	}
+	case EWeaponFiringMode::Melee:
+	{
+		FireOnce();
+		break;
+	}
+	default:
+		break;
+	}
+}
+
+void ABaseWeapon::StopFire()
+{
+	bWantsToFire = false;
+
+	if (TimerHandle_AutoFire.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(TimerHandle_AutoFire);
+	}
+
+	if (TimerHandle_Burst.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(TimerHandle_Burst);
+	}
+}
+
+void ABaseWeapon::FireOnce()
+{
+	if (!CanFire()) return;
+
+	HandleFire();
+}
+
+void ABaseWeapon::HandleFire()
+{
+
+
+	PerformHit();
+
+	ConsumeAmmo();
+
+	OnWeaponFired();
+
+	if (FiringMode == EWeaponFiringMode::SemiAuto || FiringMode == EWeaponFiringMode::Melee)
+	{
+		return;
+	}
+
+	if (FiringMode == EWeaponFiringMode::Burst)
+	{
+		float Interval = GetFireIntervalSeconds();
+		GetWorldTimerManager().SetTimer(TimerHandle_Burst, this, &ABaseWeapon::HandleFire, Interval, false);
+	}
+}
+
+void ABaseWeapon::PerformHit()
+{
+	FVector Start = GetMuzzleLocation();
+	FRotator Rot = GetMuzzleRotation();
+	FVector ShotDirection = Rot.Vector();
+
+	float HalfRad = FMath::DegreesToRadians(SpreadDegrees * 0.5f);
+	ShotDirection = FMath::VRandCone(ShotDirection, HalfRad);
+
+	FVector End = Start + ShotDirection * Range;
+
+	FHitResult Hit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+	if (GetOwner())
+	{
+		Params.AddIgnoredActor(GetOwner());
+	}
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params);
+#if WITH_EDITOR
+
+	DrawDebugLine(GetWorld(), Start, End, bHit ? FColor::Red : FColor::Green, false, 1.0f, 0, 1.0f);
+#endif
+	if (bHit)
+	{
+		AActor* HitActor = Hit.GetActor();
+		if (HitActor)
+		{
+			UGameplayStatics::ApplyPointDamage(HitActor, Damage, ShotDirection, Hit, GetInstigatorController(), this, nullptr);
+		}
+	}
+}
+
+void ABaseWeapon::ConsumeAmmo()
+{
+	if (FiringMode == EWeaponFiringMode::Melee)
+	{
+		return;
+	}
+
+	if (bInfiniteAmmo)
+	{
+		return;
+	}
+
+	if (CurrentAmmoInMagazine > 0)
+	{
+		--CurrentAmmoInMagazine;
+	}
+	else
+	{
+	}
 }
 
--- Source/HotlineMiami3D/Public/BaseWeapons.h
+++ Source/HotlineMiami3D/Public/BaseWeapons.h
@@ -1,32 +1,126 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-
+#include "GameFramework/Pawn.h"
+#include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
 #include "BaseWeapons.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponFiringMode : uint8
+{
+	Automatic	UMETA(DisplayName = "Automatic"),
+	SemiAuto	UMETA(DisplayName = "Semi-Automatic"),
+	Melee		UMETA(DisplayName = "Melee")
+};
 
 
 UCLASS(Blueprintable)
-class HOTLINEMIAMI3D_API ABaseWeapons : public AActor
+class HOTLINEMIAMI3D_API ABaseWeapon : public APawn
 {
+public:
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ABaseWeapons();
+	ABaseWeapon();
+
+protected:
+	// --- Components / internal fields required by .cpp ---
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	UStaticMeshComponent* MeshComponent = nullptr;
+
+	// --- Configurable properties ---
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	EWeaponFiringMode FiringMode = EWeaponFiringMode::SemiAuto;
+
+	// Rounds per minute
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Ranged", meta = (ClampMin = "1.0"))
+	float RateOfFireRPM = 600.0f;
+
+	// Weapon stats
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	int32 MagazineSize = 30;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	int32 CurrentAmmoInMagazine = 30;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	bool bInfiniteAmmo = false;
+
+	// Damage, range and spread
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Ranged")
+	float Damage = 25.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Ranged")
+	float Range = 10000.0f;
+
+	// Spread in degrees used by PerformHit()
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Ranged")
+	float SpreadDegrees = 1.0f;
+
+	// Melee stats
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Melee")
+	float MeleeRange = 150.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Melee")
+	float MeleeDamage = 40.0f;
+
+	// --- Internal state ---
+	FTimerHandle TimerHandle_AutoFire;
+	FTimerHandle TimerHandle_Burst;
+
+	bool bWantsToFire = false;
+
+	// track reload state & burst shots used by .cpp
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	bool bIsReloading = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	int32 CurrentBurstShot = 0;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	// helpers used by the implementation
+	virtual FVector GetMuzzleLocation() const;
+	virtual FRotator GetMuzzleRotation() const;
+	virtual float GetFireIntervalSeconds() const;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon Mesh")
-	UStaticMeshComponent* MeshComponent;
+public:
+	// Input binding helpers
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// API
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual void StartFire();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual void StopFire();
+
+	// Fire a single shot immediately (useful for semi-auto or forced single shot)
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual void FireOnce();
+
+	// Helpers
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	bool CanFire() const;
+
+	// Blueprint events
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Weapon|Events")
+	void OnWeaponFired();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Weapon|Events")
+	void OnWeaponReloaded();
+
+protected:
+
+	virtual void HandleFire();
+
+	virtual void PerformHit();
+
+	virtual void ConsumeAmmo();
 
 };
